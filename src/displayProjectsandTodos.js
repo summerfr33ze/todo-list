7722529685f2fe0c90823954eb3fcf508b8f28e2
Trevor <trevorjamesmurphy@gmail.com>
2022-09-28 16:16:14 -0400

    working through logic for displaying todo-lists

---
+++ src/displayProjectsandTodos.js
@@ -0,0 +1,71 @@
+
+
+
+function displayProjects() {
+    projectContainer.innerHTML = ""
+    projectArray.forEach((project, index) => {
+    const  projectButton = document.createElement("button")
+    projectButton.style.display = "block"
+    projectButton.textContent = project
+    projectButton.className = "project-button"
+    projectContainer.appendChild(projectButton)
+    projectButton.dataset.projectIndex = `${index}`
+    createTodoArray()
+    })
+    }
+
+// every time we create a new project
+//we create a todo page to go along
+//we create new todo array
+// we add that array to an array of todo arrays
+
+//when we click a project button
+//we clear the todo container
+//we select the specific todo array
+// we load the specific todos into the page
+
+function createTodoArray() {
+    const todoArray = []
+    todoArray.push(arrayOfTodoArrays)
+    todoArray.dataset.todoArrayIndex = `${index}`
+
+}
+
+function displayTodos(){
+todoContainer.innerHTML = ""
+arrayOfTodoArrays[i].forEach((todoArray, index) => {
+const todoDiv = document.createElement("div")
+todoDiv.className = "todo-div"
+todoContainer.appendChild(todoDiv)
+for(let key in Todo){
+    const entry = document.createElement("div")
+    todoDiv.appendChild(entry)
+    entry.textContent = `${key} : ${Todo[key]} `
+}
+const todoDeleteButton = document.createElement("button")
+todoDeleteButton.className = "delete"
+todoDiv.appendChild(todoDeleteButton)
+
+todoDeleteButton.textContent = "Delete"
+    todoDeleteButton.addEventListener("click", (event) => {
+    todoDeleteButton.innerHTML = ""
+    const attr = todoDeleteButton.parentElement.getAttribute("data-todo-index")
+    todoArray.splice(attr, 1)
+    displayTodos()
+    })
+})
+
+
+}
+
+class Todo {
+    constructor(Title,Description,DueDate,Priority){
+        this.title = Title
+        this.description = Description
+        this.dueDate = DueDate
+        this.priority = Priority
+        }
+    }
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,87 @@
 import "./style.css"
-import DOMModule from "./DOMModule.js"
 
-DOMModule()
+//Areas of the Site
+const projectPanel = document.querySelector(".project-panel")
+const toDoArea = document.querySelector(".todo-area")
+
+// project titles are placed above where form appears
+const projectContainer = document.querySelector(".project-container")
+const formContainer = document.querySelector(".form-container")
+const projectForm = document.querySelector(".project-form")
+
+
+
+//project input elements
+const popupInput = document.querySelector(".popup-input")
+const add = document.querySelector(".add")
+const cancel = document.querySelector(".cancel")
+
+
+
+//todo  DOM elements
+const title = document.querySelector(".title")
+const description= document.querySelector(".description")
+const dueDate = document.querySelector(".due-date")
+const priority= document.querySelector(".priority")
+const addTodo = document.querySelector(".add-todo")
+
+
+
+/*function displayTodos(){
+todoContainer.innerHTML = ""
+todoArray.forEach((todo, index) => {
+const todoDiv = document.createElement("div")
+todoDiv.className = "todo-div"
+}
+}
+
+function deleteProject(){
+    const deleteProject = document.createElement("div")
+}
+*/
+
+
+
+
+const projectArray = []
+const arrayOfTodoArrays = []
+
+
+// user adds project
+
+
+
+add.addEventListener("click", (event) => {
+    event.preventDefault()
+    const project = popupInput.value
+    projectArray.push(project)
+    displayProjects()
+    projectForm.reset()
+    
+})
+
+cancel.addEventListener("click", (event) => {
+    event.preventDefault()
+    projectForm.reset()
+    
+
+})
+
+projectButton.addEventListener("click", () => {
+    todoContainer.innerHTML = ""
+    i = projectButton.getAttribute("data-project-index")
+    displayTodos()
+})
+
+
+/*
+  addEventListener("click", (event) => {
+    event.preventDefault()
+    const todo = new Todo (title.value,description.value,dueDate.value, priority.value)
+    todoArray.push(todo)
+    displayTodos()
+    closeForm()
+
+})
+
+*/
